80f4d9594791630b928aac85a70161a8983130bb
99 <hahahoho5915@gmail.com>
2025-04-18 19:18:38 +0900

    feat: kafka config 수정

--- src/main/java/com/kk/cschat/config/KafkaProducerConfig.java
+++ src/main/java/com/kk/cschat/config/KafkaProducerConfig.java
@@ -3,8 +3,10 @@ package com.kk.cschat.config;
 import com.kk.cschat.kafka.dto.KafkaMessage;
 import java.util.HashMap;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,7 +15,10 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaProducerConfig {
+
+    private final KafkaProperties kafkaProperties;
 
     @Bean
     public KafkaTemplate<String, KafkaMessage> kafkaMessageTemplate() {
@@ -23,7 +28,7 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, KafkaMessage> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
